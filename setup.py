34b6beae62a8b9baa7817aa1f8ca236acd0fa9ee
bendmorris <ben@bendmorris.com>
2013-03-19 21:29:21 -0400

    Moving Python modules to pyj2d directory.

--- setup.py
+++ setup.py
@@ -5,7 +5,4 @@ setup(name='pyj2d',
       author='James Garnon',
       url='http://gatc.ca/projects/pyj2d/',
       packages=['pyj2d'],
-      package_dir={
-                'pyj2d':''
-                },
       )
