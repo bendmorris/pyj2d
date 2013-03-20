76b53ffb898434a174b10a6fa7082d1c310239ad
bendmorris <ben@bendmorris.com>
2013-03-19 21:20:50 -0400

    Adding setup script so that the library doesn't need to be copied to each project that uses it.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='pyj2d',
+      author='James Garnon',
+      url='http://gatc.ca/projects/pyj2d/',
+      packages=['pyj2d'],
+      package_dir={
+                'pyj2d':''
+                },
+      )
